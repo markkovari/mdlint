158e9305d72c0d9d2c87c309d39468b6d2bcdd78
markkovari <kovarimarkofficial@gmail.com>
2023-04-13 21:09:47 +0200

    feat: get links

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use comrak::{
-    nodes::{AstNode, NodeCode, NodeValue},
-    parse_document, Arena, ComrakOptions,
-};
 use jwalk::WalkDir;
+use pulldown_cmark::{html, Event, Options, Parser, Tag};
 use std::{env::args, io::Result};
 
 const EXTENSIONS: [&str; 2] = ["md", "markdown"];
@@ -11,38 +8,18 @@ fn ends_with_extension(path: &str) -> bool {
     EXTENSIONS.iter().any(|ext| path.ends_with(ext))
 }
 
-fn get_document_title(document: &str) -> String {
-    let arena = Arena::new();
-    let root = parse_document(&arena, document, &ComrakOptions::default());
-
-    for node in root.children() {
-        let header = match node.data.clone().into_inner().value {
-            NodeValue::Heading(c) => c,
-            _ => continue,
-        };
-        if header.level != 1 {
-            continue;
+fn get_document_link(document: &str) -> Vec<(String, String)> {
+    let mut links: Vec<(String, String)> = Vec::new();
+    let parser = Parser::new_ext(document, Options::empty()).map(|event| match event {
+        Event::Start(Tag::Link(a, url, title)) => {
+            links.push((url.to_string(), title.to_string()));
+            Event::Start(Tag::Link(a, url, title))
         }
-        let mut text = String::new();
-        collect_text(node, &mut text);
-        return text;
-    }
-
-    "Untitled Document".to_string()
-}
-
-fn collect_text<'a>(node: &'a AstNode<'a>, output: &mut String) {
-    match node.data.borrow().value {
-        NodeValue::Text(ref literal) | NodeValue::Code(NodeCode { ref literal, .. }) => {
-            output.push_str(literal)
-        }
-        NodeValue::LineBreak | NodeValue::SoftBreak => output.push(' '),
-        _ => {
-            for n in node.children() {
-                collect_text(n, output);
-            }
-        }
-    }
+        _ => event,
+    });
+    let mut res = String::new();
+    html::push_html(&mut res, parser);
+    links
 }
 
 fn main() -> Result<()> {
@@ -57,8 +34,9 @@ fn main() -> Result<()> {
             if ends_with_extension(entry.path().to_str().unwrap()) {
                 println!("{}", entry.path().display());
                 let file_content = std::fs::read_to_string(entry.path())?;
-                let title = get_document_title(&file_content);
-                println!("title: {}", title);
+                for link in get_document_link(&file_content) {
+                    println!("Link: {:?}", link);
+                }
             }
         }
     }
